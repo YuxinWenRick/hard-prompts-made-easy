f679eec0b21a2e96d8eb458f4f210f3112ec9bbc
farazamjad <faraz.amjad007@gmail.com>
2023-02-23 23:18:02 +0500

    pylint improved

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+from optim_utils import *
+import open_clip
+import argparse
 import sys
 from PIL import Image
 
 if len(sys.argv) < 2:
-  sys.exit("""Usage: python run.py path-to-image [path-to-image-2 ...]
+    sys.exit("""Usage: python run.py path-to-image [path-to-image-2 ...]
 Passing multiple images will optimize a single prompt across all passed images, useful for style transfer.
 """)
 
@@ -14,9 +17,6 @@ image_paths = sys.argv[1:]
 images = [Image.open(image_path) for image_path in image_paths]
 
 # defer loading other stuff until we confirm the images loaded
-import argparse
-import open_clip
-from optim_utils import *
 
 print("Initializing...")
 
@@ -29,12 +29,19 @@ args.print_new_best = True
 
 # load CLIP model
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, _, preprocess = open_clip.create_model_and_transforms(args.clip_model, pretrained=args.clip_pretrain, device=device)
+model, _, preprocess = open_clip.create_model_and_transforms(
+    args.clip_model, pretrained=args.clip_pretrain, device=device)
 
 print(f"Running for {args.iter} steps.")
 if getattr(args, 'print_new_best', False) and args.print_step is not None:
-  print(f"Intermediate results will be printed every {args.print_step} steps.")
+    print(
+        f"Intermediate results will be printed every {args.print_step} steps.")
 
 # optimize prompt
-learned_prompt = optimize_prompt(model, preprocess, args, device, target_images=images)
+learned_prompt = optimize_prompt(
+    model,
+    preprocess,
+    args,
+    device,
+    target_images=images)
 print(learned_prompt)
