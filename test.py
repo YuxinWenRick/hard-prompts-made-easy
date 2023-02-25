1401796c5335cb0ef89f25d9ae16d5653f0e7d55
farazamjad <81928514+farazamjad@users.noreply.github.com>
2023-02-26 00:06:16 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import torch
-from tokenizer import tokenize
+from open_clip.tokenizer import tokenize
 def test_tokenize():
     expected_result = torch.tensor([[49408, 364, 16, 939, 4054, 4, 175, 49409, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[49408, 364, 16, 939, 4054, 4, 175, 49409, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
     result = tokenize(["A photo of a family of ducks walking down the street"])
