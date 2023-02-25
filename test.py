a5b57cb4c883a46f3e6a32deb8dff9c609b3decc
farazamjad <81928514+farazamjad@users.noreply.github.com>
2023-02-25 23:45:25 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,5 @@
-import unittest
-from typing import Dict
-
-def bytes_to_unicode() -> Dict[int, str]:
-    # function implementation goes here
-
-    class TestBytesToUnicode(unittest.TestCase):
-        def test_output_type(self):
-            result = bytes_to_unicode()
-            self.assertIsInstance(result, dict)
-        
-        def test_output_length(self):
-            result = bytes_to_unicode()
-            self.assertGreaterEqual(len(result), 256)
-        
-        def test_valid_keys(self):
-            result = bytes_to_unicode()
-            for key in range(256):
-                self.assertIn(key, result)
-            
-        def test_valid_values(self):
-            result = bytes_to_unicode()
-            for value in result.values():
-                self.assertIsInstance(value, str)
-            
-if __name__ == '__main__':
-    unittest.main()
+def test_tokenize():
+    texts = ["This is a test.", "This is another test."]
+    expected_result = torch.tensor([[49408,   364,    16,   939,  4054,     4,   175, 49409,     0, ... 0],
+                                    [49408,   364,    16,   939,  4054,    38,   308,  4413,  4054, ... 0]])
+    assert torch.allclose(tokenize(text
