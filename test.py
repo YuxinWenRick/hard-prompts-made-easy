6a32fd8b48c3bea8d0a43e70818600c6e0daaa71
farazamjad <81928514+farazamjad@users.noreply.github.com>
2023-02-25 22:56:57 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,38 @@
-import os
-import pytest
-from PIL import Image
-from run import optimize_image
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import sys
 
-# Set the path to the test image
-IMAGE_PATH = "test-image.jpeg"
+# Import the function to be tested
+from optim_utils import optimize_prompt
 
-def test_optimize_image():
-    # Load the test image
-    image = Image.open(IMAGE_PATH)
 
-    # Optimize the prompt for the image
-    learned_prompt = optimize_image(image)
+class TestOptimizePrompt(unittest.TestCase):
+    @patch('optim_utils.read_json')
+    @patch('open_clip.create_model_and_transforms')
+    @patch('sys.argv', ['test.py', 'path-to-image'])
+    @patch('builtins.print')
+    def test_optimize_prompt(self, mock_print, mock_model, mock_json):
+        # Mock the return value of read_json
+        mock_json.return_value = {'clip_pretrain': True, 'clip_model': 'ViT-B/32', 'lr': 0.1, 'step_size': 10,
+                                  'gamma': 0.9, 'decay_rate': 0.99, 'kl_weight': 0.05, 'kl_weight_increase': 0.00005,
+                                  'kl_tolerance': 0.5, 'init_weight': 0.1, 'init_noise': 0.0, 'init_size': 5000,
+                                  'randomize_noise': False, 'optimizer': 'Adam', 'optim_kwargs': {'betas': (0.9, 0.999)},
+                                  'clip_guidance_scale': 0.0, 'tv_scale': 0.1, 'l2_scale': 0.0,
+                                  'clip_model_params': {'dim': 512, 'n_heads': 8, 'n_layers': 12, 'vocab_size': 49408},
+                                  'iterations': 2000, 'save_every': 100, 'save_progress': False,
+                                  'output_path': 'outputs', 'print_step': 100, 'seed': None}
 
-    # Assert that the learned prompt is not empty
-    assert learned_prompt
+        # Mock the return value of create_model_and_transforms
+        mock_model.return_value = (None, None, None)
 
-    # Assert that the learned prompt is a string
-    assert isinstance(learned_prompt, str)
+        # Mock the return value of optimize_prompt
+        mock_prompt = 'Learned prompt'
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            with patch('optim_utils.optimize_prompt', return_value=mock_prompt):
+                learned_prompt = optimize_prompt(None, None, None, None, None)
 
-    # Assert that the learned prompt contains no whitespace
-    assert not any(char.isspace() for char in learned_prompt)
+        self.assertEqual(learned_prompt, mock_prompt)
+
+if __name__ == '__main__':
+    unittest.main()
