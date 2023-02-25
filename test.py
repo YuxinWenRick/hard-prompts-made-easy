1736a5b42b82e535c9ea13de28b9f225b3b958f2
farazamjad <81928514+farazamjad@users.noreply.github.com>
2023-02-25 23:50:13 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ def test_tokenize():
     texts = ["This is a test.", "This is another test."]
     expected_result = torch.tensor([[49408,   364,    16,   939,  4054,     4,   175, 49409,     0, ... 0],
                                     [49408,   364,    16,   939,  4054,    38,   308,  4413,  4054, ... 0]])
-    assert torch.allclose(tokenize(text
+    assert torch.allclose(tokenize(texts), expected_result)
