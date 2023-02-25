ffea28ecc89ec9c20299cd354ff774e36b4ecf58
farazamjad <81928514+farazamjad@users.noreply.github.com>
2023-02-25 23:01:03 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,42 @@
-import unittest
-from unittest.mock import patch
-from io import StringIO
-import sys
-
-# Import the function to be tested
-from optim_utils import optimize_prompt
-
+from unittest import mock
 
 class TestOptimizePrompt(unittest.TestCase):
-    @patch('optim_utils.read_json')
-    @patch('open_clip.create_model_and_transforms')
-    @patch('sys.argv', ['test.py', 'path-to-image'])
-    @patch('builtins.print')
-    def test_optimize_prompt(self, mock_print, mock_model, mock_json):
-        # Mock the return value of read_json
-        mock_json.return_value = {'clip_pretrain': True, 'clip_model': 'ViT-B/32', 'lr': 0.1, 'step_size': 10,
-                                  'gamma': 0.9, 'decay_rate': 0.99, 'kl_weight': 0.05, 'kl_weight_increase': 0.00005,
-                                  'kl_tolerance': 0.5, 'init_weight': 0.1, 'init_noise': 0.0, 'init_size': 5000,
-                                  'randomize_noise': False, 'optimizer': 'Adam', 'optim_kwargs': {'betas': (0.9, 0.999)},
-                                  'clip_guidance_scale': 0.0, 'tv_scale': 0.1, 'l2_scale': 0.0,
-                                  'clip_model_params': {'dim': 512, 'n_heads': 8, 'n_layers': 12, 'vocab_size': 49408},
-                                  'iterations': 2000, 'save_every': 100, 'save_progress': False,
-                                  'output_path': 'outputs', 'print_step': 100, 'seed': None}
-
-        # Mock the return value of create_model_and_transforms
-        mock_model.return_value = (None, None, None)
-
-        # Mock the return value of optimize_prompt
-        mock_prompt = 'Learned prompt'
-        with patch('sys.stdout', new=StringIO()) as fake_stdout:
-            with patch('optim_utils.optimize_prompt', return_value=mock_prompt):
-                learned_prompt = optimize_prompt(None, None, None, None, None)
-
-        self.assertEqual(learned_prompt, mock_prompt)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_optimize_prompt(self):
+        # create mock objects
+        model = mock.Mock()
+        model.token_embedding = torch.nn.Embedding(512, 512)
+        preprocess = mock.Mock()
+        args = argparse.Namespace()
+        args.clip_model = "ViT-B/32"
+        args.clip_pretrain = True
+        args.iter = 10
+        args.lr = 0.1
+        args.init_weight = 0.1
+        args.optim = "adam"
+        args.clip_norm = 1.0
+        args.tv_norm = 0.1
+        args.l2_norm = 0.01
+        args.weight_decay = 0.01
+        args.print_step = 1
+        args.init_image = None
+        args.init_random = True
+        args.init_size = 224
+        args.mixed_precision = False
+        args.clip_guidance_scale = 0.0
+        args.color_correlation = 0.0
+        args.color_correlation_temperature = 0.0
+        args.normalize_image = True
+        args.jitter = True
+        args.center_bias = False
+        args.clip_min = -1.0
+        args.clip_max = 1.0
+        args.batch_size = 1
+        args.print_new_best = True
+        device = "cpu"
+        target_images = [torch.randn(3, 224, 224)]
+        
+        # call optimize_prompt
+        learned_prompt = optimize_prompt(model, preprocess, args, device, target_images=target_images)
+        
+        # assert the output
+        self.assertIsNotNone(learned_prompt)
