c23efe70d3f3969d6b65dc2128946bc8fbd1a60c
farazamjad <81928514+farazamjad@users.noreply.github.com>
2023-02-25 23:36:11 +0500

    Update test.py

--- test.py
+++ test.py
@@ -4,24 +4,24 @@ from typing import Dict
 def bytes_to_unicode() -> Dict[int, str]:
     # function implementation goes here
 
-class TestBytesToUnicode(unittest.TestCase):
-    def test_output_type(self):
-        result = bytes_to_unicode()
-        self.assertIsInstance(result, dict)
+    class TestBytesToUnicode(unittest.TestCase):
+        def test_output_type(self):
+            result = bytes_to_unicode()
+            self.assertIsInstance(result, dict)
         
-    def test_output_length(self):
-        result = bytes_to_unicode()
-        self.assertGreaterEqual(len(result), 256)
+        def test_output_length(self):
+            result = bytes_to_unicode()
+            self.assertGreaterEqual(len(result), 256)
         
-    def test_valid_keys(self):
-        result = bytes_to_unicode()
-        for key in range(256):
-            self.assertIn(key, result)
+        def test_valid_keys(self):
+            result = bytes_to_unicode()
+            for key in range(256):
+                self.assertIn(key, result)
             
-    def test_valid_values(self):
-        result = bytes_to_unicode()
-        for value in result.values():
-            self.assertIsInstance(value, str)
+        def test_valid_values(self):
+            result = bytes_to_unicode()
+            for value in result.values():
+                self.assertIsInstance(value, str)
             
 if __name__ == '__main__':
     unittest.main()
